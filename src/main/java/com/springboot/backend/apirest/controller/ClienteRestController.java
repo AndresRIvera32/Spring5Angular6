9896af1974b6cbae69fbe67ca68a0721dd4f7759
Andres Rivera <andres-illidan92@hotmail.com>
2018-08-13 11:04:50 -0500

    Creacion de RestController y data de prueba

---
+++ src/main/java/com/springboot/backend/apirest/controller/ClienteRestController.java
@@ -0,0 +1,29 @@
+package com.springboot.backend.apirest.controller;
+
+import java.util.List;
+
+import javax.xml.ws.RespectBinding;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.backend.apirest.models.entity.Cliente;
+import com.springboot.backend.apirest.service.ClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+	
+	@Autowired
+	@Qualifier("clienteServiceImpl")
+	private ClienteService clientService;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> getCliente(){
+		return clientService.findAll();
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into clientes values(1,'rodriguez',CURDATE(),'andres@mail.com','andres');
+
+insert into clientes values(2,'rodriguez',CURDATE(),'andres@mail.com','andres');
+insert into clientes values(3,'pinzon',CURDATE(),'ana@mail.com','ana');
+insert into clientes values(4,'lara',CURDATE(),'sancas@mail.com','sancas');
+insert into clientes values(5,'cañon',CURDATE(),'catalina@mail.com','catalina');
+insert into clientes values(6,'sandoval',CURDATE(),'sara@mail.com','sara');
+insert into clientes values(7,'prieto',CURDATE(),'carlos@mail.com','carlos');
