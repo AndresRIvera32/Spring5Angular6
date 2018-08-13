7ea4ece8fe6596a21cb351f0581c7c0cc3247340
Andres Rivera <andres-illidan92@hotmail.com>
2018-08-13 11:25:12 -0500

    @CrossOrigi determina que dominio esta permitido realizar request y tipo determina que dominio puede realizar request y que tipo de request esta permitido GET,POST,PUT,DELETE

--- src/main/java/com/springboot/backend/apirest/controller/ClienteRestController.java
+++ src/main/java/com/springboot/backend/apirest/controller/ClienteRestController.java
@@ -6,6 +6,7 @@ import javax.xml.ws.RespectBinding;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.backend.apirest.models.entity.Cliente;
 import com.springboot.backend.apirest.service.ClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
